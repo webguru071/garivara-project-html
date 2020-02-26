a008f6d6216fd5b088a9cd77bb867a4622aebe0e
Hedayet@AndIT <webguru071@gmail.com>
2020-02-26 16:34:01 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-
+// Main Slider Carousel
+if ($(".banner-carousel").length) {
+  $(".banner-carousel").owlCarousel({
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
+    loop: true,
+    margin: 0,
+    nav: false,
+    singleItem: true,
+    smartSpeed: 500,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    navText: [
+      '<span class="fas fa-angle-left"></span>',
+      '<span class="fas fa-angle-right"></span>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    }
+  });
+}
