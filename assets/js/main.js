cd00c93ae788a145cdbcf2edbd37ca773bc7b411
Hedayet@AndIT <webguru071@gmail.com>
2020-02-29 19:30:33 +0600

    home page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,59 @@ $(".agent-items-wrapper").owlCarousel({
     }
   }
 });
+
+$(".customer-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  nav: false,
+  items: 4,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1
+    },
+    576: {
+      items: 1
+    },
+    1920: {
+      items: 1
+    },
+    1200: {
+      items: 1
+    }
+  }
+});
+
+$(".logo-wrapper").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  nav: false,
+  items: 4,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1
+    },
+    576: {
+      items: 4
+    },
+    1920: {
+      items: 6
+    },
+    1200: {
+      items: 6
+    }
+  }
+});
