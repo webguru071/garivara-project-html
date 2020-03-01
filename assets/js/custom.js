27fa69d3efce795cbc8d558a8e176f26f2d1e86d
Hedayet@AndIT <webguru071@gmail.com>
2020-03-01 19:04:11 +0600

    Upadate Home Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,117 +1,171 @@
-// (function($) {
-//   "use strict";
-
-//   var digiciaApp = {
-//     /* ---------------------------------------------
-//             ## Mobile Menu
-//         --------------------------------------------- */
-//     mobile_menu: function() {
-//       var mobilesearch = $(
-//         ".site-header .navigation-area .header-navigation-right"
-//       )
-//         .clone()
-//         .appendTo(".mobile-menu");
-//       // mobile Menu
-//       //-------------------------------
-//       $(".site-navigation .mainmenu-area nav").meanmenu({
-//         meanMenuClose: "<i class='fas fa-times'></i>",
-//         meanMenuCloseSize: "18px",
-//         meanScreenWidth: "1199",
-//         meanExpandableChildren: true,
-//         meanMenuContainer: ".mobile-menu-area .mobile-menu"
-//       });
-//     },
-
-// })(jQuery);
-
-(function($) {
-  "use strict";
-
-  var digiciaApp = {
-    /* ---------------------------------------------
-            ## Mobile Menu
-        --------------------------------------------- */
-    mobile_menu: function() {
-      var mobilesearch = $(
-        ".site-header .navigation-area .header-navigation-right"
-      )
-        .clone()
-        .appendTo(".mobile-menu");
-      // mobile Menu
-      //-------------------------------
-      $(".site-navigation .mainmenu-area nav").meanmenu({
-        meanMenuClose: "<i class='fas fa-times'></i>",
-        meanMenuCloseSize: "18px",
-        meanScreenWidth: "1199",
-        meanExpandableChildren: true,
-        meanMenuContainer: ".mobile-menu-area .mobile-menu"
-      });
-    },
-
-    /* ---------------------------------------------
-		 function initializ
-		 --------------------------------------------- */
-    initializ: function() {
-      digiciaApp.mobile_menu();
+// Main Slider Carousel
+if ($(".banner-carousel").length) {
+  $(".banner-carousel").owlCarousel({
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
+    loop: true,
+    margin: 0,
+    nav: false,
+    singleItem: true,
+    smartSpeed: 500,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    navText: [
+      '<span class="fas fa-angle-left"></span>',
+      '<span class="fas fa-angle-right"></span>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
     }
-  };
-  /* ---------------------------------------------
-	 Document ready function
-	 --------------------------------------------- */
-  $(function() {
-    digiciaApp.initializ();
   });
-})(jQuery);
+}
 
+/*----------------------------------------
+         Agent Slider
+----------------------------------------*/
 
+$(".agent-items-wrapper").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  nav: false,
+  items: 4,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1
+    },
+    576: {
+      items: 3
+    },
+    1920: {
+      items: 4
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
 
-     /*----------------------------------------
+/*----------------------------------------
+         Customers Slider
+----------------------------------------*/
+
+$(".customer-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  nav: false,
+  items: 4,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1
+    },
+    576: {
+      items: 1
+    },
+    1920: {
+      items: 1
+    },
+    1200: {
+      items: 1
+    }
+  }
+});
+/*----------------------------------------
+           Logo Slider
+----------------------------------------*/
+$(".logo-wrapper").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  nav: false,
+  items: 4,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1
+    },
+    576: {
+      items: 4
+    },
+    1920: {
+      items: 6
+    },
+    1200: {
+      items: 6
+    }
+  }
+});
+/*----------------------------------------
             Video Plugin (About Page)
-        ----------------------------------------*/
+----------------------------------------*/
 
-    var $iframe = $('iframe'),
-        $videoLink = $('.video-link'),
-        playerTemplate =
-        '<div class="player"><div class="player__video"><div class="video-filler"></div><button class="video-close">&times;</button><iframe class="video-iframe" src="{{iframevideo}}" frameborder="0" allowfullscreen></iframe></div><div/>';
+var $iframe = $("iframe"),
+  $videoLink = $(".video-link"),
+  playerTemplate =
+    '<div class="player"><div class="player__video"><div class="video-filler"></div><button class="video-close">&times;</button><iframe class="video-iframe" src="{{iframevideo}}" frameborder="0" allowfullscreen></iframe></div><div/>';
 
-    $videoLink.on('click', function (e) {
-        var localTemplate = '',
-            videoWidth = parseInt($(this).data('width')),
-            videoHeight = parseInt($(this).data('height')),
-            videoAspect = (videoHeight / videoWidth) * 100,
-            // elements
-            $player = null,
-            $video = null,
-            $close = null,
-            $iframe = null;
+$videoLink.on("click", function(e) {
+  var localTemplate = "",
+    videoWidth = parseInt($(this).data("width")),
+    videoHeight = parseInt($(this).data("height")),
+    videoAspect = (videoHeight / videoWidth) * 100,
+    // elements
+    $player = null,
+    $video = null,
+    $close = null,
+    $iframe = null;
 
-        e.preventDefault();
+  e.preventDefault();
 
-        localTemplate = playerTemplate.replace(
-            '{{iframevideo}}',
-            $(this).prop('href')
-        );
+  localTemplate = playerTemplate.replace(
+    "{{iframevideo}}",
+    $(this).prop("href")
+  );
 
-        $player = $(localTemplate);
+  $player = $(localTemplate);
 
-        $player.find('.video-filler').css('padding-top', videoAspect + '%');
+  $player.find(".video-filler").css("padding-top", videoAspect + "%");
 
-        $close = $player.find('.video-close').on('click', function () {
-            $(this)
-                .off()
-                .closest('.player')
-                .hide()
-                .remove();
-        });
+  $close = $player.find(".video-close").on("click", function() {
+    $(this)
+      .off()
+      .closest(".player")
+      .hide()
+      .remove();
+  });
 
-        $player.appendTo('body').addClass('js--show-video');
-    })  
+  $player.appendTo("body").addClass("js--show-video");
+});
 
-
- /*----------------------------------------
+/*----------------------------------------
         AccordionPlugin (About Page)
  ----------------------------------------*/
-
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -120,10 +174,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,116 +1,38 @@
-// Main Slider Carousel
-if ($(".banner-carousel").length) {
-  $(".banner-carousel").owlCarousel({
-    animateOut: "fadeOut",
-    animateIn: "fadeIn",
-    loop: true,
-    margin: 0,
-    nav: false,
-    singleItem: true,
-    smartSpeed: 500,
-    autoplay: true,
-    autoplayTimeout: 6000,
-    navText: [
-      '<span class="fas fa-angle-left"></span>',
-      '<span class="fas fa-angle-right"></span>'
-    ],
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1024: {
-        items: 1
-      }
+(function($) {
+  "use strict";
+
+  var digiciaApp = {
+    /* ---------------------------------------------
+			  ## Mobile Menu
+		  --------------------------------------------- */
+    mobile_menu: function() {
+      var mobilesearch = $(
+        ".site-header .navigation-area .header-navigation-right"
+      )
+        .clone()
+        .appendTo(".mobile-menu");
+      // mobile Menu
+      //-------------------------------
+      $(".site-navigation .mainmenu-area nav").meanmenu({
+        meanMenuClose: "<i class='fas fa-times'></i>",
+        meanMenuCloseSize: "18px",
+        meanScreenWidth: "1199",
+        meanExpandableChildren: true,
+        meanMenuContainer: ".mobile-menu-area .mobile-menu"
+      });
+    },
+
+    /* ---------------------------------------------
+		   function initializ
+		   --------------------------------------------- */
+    initializ: function() {
+      digiciaApp.mobile_menu();
     }
+  };
+  /* ---------------------------------------------
+	   Document ready function
+	   --------------------------------------------- */
+  $(function() {
+    digiciaApp.initializ();
   });
-}
-
-
-
-
-$(".agent-items-wrapper").owlCarousel({
-  loop: true,
-  margin: 0,
-  dots: true,
-  nav: false,
-  items: 4,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    360: {
-      items: 1
-    },
-    576: {
-      items: 3
-    },
-    1920: {
-      items: 4
-    },
-    1200: {
-      items: 4
-    }
-  }
-});
-
-$(".customer-slider").owlCarousel({
-  loop: true,
-  margin: 0,
-  dots: true,
-  nav: false,
-  items: 4,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    360: {
-      items: 1
-    },
-    576: {
-      items: 1
-    },
-    1920: {
-      items: 1
-    },
-    1200: {
-      items: 1
-    }
-  }
-});
-
-$(".logo-wrapper").owlCarousel({
-  loop: true,
-  margin: 0,
-  dots: true,
-  nav: false,
-  items: 4,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    360: {
-      items: 1
-    },
-    576: {
-      items: 4
-    },
-    1920: {
-      items: 6
-    },
-    1200: {
-      items: 6
-    }
-  }
-});
+})(jQuery);
