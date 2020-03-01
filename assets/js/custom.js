6b89f7565b87d62d0d9bd87616b727d6b7370036
Hedayet@AndIT <webguru071@gmail.com>
2020-03-01 16:46:45 +0600

    Home Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,29 @@
+// (function($) {
+//   "use strict";
+
+//   var digiciaApp = {
+//     /* ---------------------------------------------
+//             ## Mobile Menu
+//         --------------------------------------------- */
+//     mobile_menu: function() {
+//       var mobilesearch = $(
+//         ".site-header .navigation-area .header-navigation-right"
+//       )
+//         .clone()
+//         .appendTo(".mobile-menu");
+//       // mobile Menu
+//       //-------------------------------
+//       $(".site-navigation .mainmenu-area nav").meanmenu({
+//         meanMenuClose: "<i class='fas fa-times'></i>",
+//         meanMenuCloseSize: "18px",
+//         meanScreenWidth: "1199",
+//         meanExpandableChildren: true,
+//         meanMenuContainer: ".mobile-menu-area .mobile-menu"
+//       });
+//     },
+
+// })(jQuery);
+
 (function($) {
   "use strict";
 
@@ -22,4 +48,17 @@
       });
     },
 
+    /* ---------------------------------------------
+		 function initializ
+		 --------------------------------------------- */
+    initializ: function() {
+      digiciaApp.mobile_menu();
+    }
+  };
+  /* ---------------------------------------------
+	 Document ready function
+	 --------------------------------------------- */
+  $(function() {
+    digiciaApp.initializ();
+  });
 })(jQuery);
